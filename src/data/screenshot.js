45c7133dd9690fdd9151bc5a65c8bade56ced9d8
Mukul Goel <mukulgoel791@gmail.com>
2025-04-05 18:56:34 +0530

    Create new file for handling screenshort and imported in portfolio.js

---
+++ src/data/screenshot.js
@@ -0,0 +1,59 @@
+// KBFC Screenshots
+import ss1 from "../assets/KBFC Screenshot/Screenshot_1.png";
+import ss2 from "../assets/KBFC Screenshot/Screenshot_2.png";
+import ss3 from "../assets/KBFC Screenshot/Screenshot_3.png";
+import ss4 from "../assets/KBFC Screenshot/Screenshot_4.png";
+import ss5 from "../assets/KBFC Screenshot/Screenshot_5.png";
+import ss6 from "../assets/KBFC Screenshot/Screenshot_6.png";
+import ss7 from "../assets/KBFC Screenshot/Screenshot_7.png";
+import ss8 from "../assets/KBFC Screenshot/Screenshot_8.png";
+import ss9 from "../assets/KBFC Screenshot/Screenshot_9.png";
+import ss10 from "../assets/KBFC Screenshot/Screenshot_10.png";
+import ss11 from "../assets/KBFC Screenshot/Screenshot_11.png";
+
+// Factory ERP Screenshots
+import s1 from "../assets/Factory_erp_screenshot/Screenshot_1.png";
+import s2 from "../assets/Factory_erp_screenshot/Screenshot_2.png";
+import s3 from "../assets/Factory_erp_screenshot/Screenshot_3.png";
+import s4 from "../assets/Factory_erp_screenshot/Screenshot_4.png";
+import s5 from "../assets/Factory_erp_screenshot/Screenshot_5.png";
+import s6 from "../assets/Factory_erp_screenshot/Screenshot_6.png";
+import s7 from "../assets/Factory_erp_screenshot/Screenshot_7.png";
+import s8 from "../assets/Factory_erp_screenshot/Screenshot_8.png";
+import s9 from "../assets/Factory_erp_screenshot/Screenshot_9.png";
+import s10 from "../assets/Factory_erp_screenshot/Screenshot_10.png";
+import s11 from "../assets/Factory_erp_screenshot/Screenshot_11.png";
+import s12 from "../assets/Factory_erp_screenshot/Screenshot_12.png";
+import s13 from "../assets/Factory_erp_screenshot/Screenshot_13.png";
+import s14 from "../assets/Factory_erp_screenshot/Screenshot_14.png";
+
+export const kbfcscreenshots = [
+  { src: ss1, title: "Login Page" },
+  { src: ss2, title: "Dashboard" },
+  { src: ss3, title: "Purchase List" },
+  { src: ss4, title: "Purchase Info" },
+  { src: ss5, title: "Tour and Travel List" },
+  { src: ss6, title: "Tour and Travel Info" },
+  { src: ss7, title: "Expense List" },
+  { src: ss8, title: "Create Expense Request" },
+  { src: ss9, title: "Modal to upload expense" },
+  { src: ss10, title: "Expense/Advance Info" },
+  { src: ss11, title: "Profile" }
+];
+
+export const factoryscreenshots = [
+  { src: s1, title: "Login Page" },
+  { src: s2, title: "Dashboard" },
+  { src: s3, title: "Self Attendance" },
+  { src: s4, title: "Attendance check in - geographic check" },
+  { src: s5, title: "Attendance History" },
+  { src: s6, title: "Site attendance -  Employee Wise" },
+  { src: s7, title: "Select date and site for DPR" },
+  { src: s8, title: "PO Select for DPR" },
+  { src: s9, title: "DPR List" },
+  { src: s10, title: "Employee select for DPR Entry" },
+  { src: s11, title: "DPR Filling acc to Service/Item" },
+  { src: s12, title: "Expense/Advance List" },
+  { src: s13, title: "Create Expense/Advance Request" },
+  { src: s14, title: "Profile Page" }
+];
