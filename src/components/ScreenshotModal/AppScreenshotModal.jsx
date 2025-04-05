8a65cce5bc18b105d2d8cba9adca86d32813a9a8
Mukul Goel <mukulgoel791@gmail.com>
2025-04-05 20:17:17 +0530

    added loading in model and fixed profile image for mobile view

--- src/components/ScreenshotModal/AppScreenshotModal.jsx
+++ src/components/ScreenshotModal/AppScreenshotModal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -10,31 +10,56 @@ const AppScreenshotModal = ({ isOpen, closeModal, images }) => {
 
   return (
     <div className="modal-overlay">
-        <div className="modal-panel">
-            <button className="modal-close-icon" onClick={closeModal} >
-            &times;
-            </button>
+      <div className="modal-panel">
+        <button className="modal-close-icon" onClick={closeModal}>
+          &times;
+        </button>
 
-            <h2 className="modal-title" style={{color:'#fff'}}>App Screenshots</h2>
-            <Swiper
-            spaceBetween={20}
-            slidesPerView={1}
-            loop={images.length > 1}
-            navigation
-            modules={[Navigation]}
-            >
-            {images.map((img, index) => (
-                <SwiperSlide key={index}>
-                    <div className="modal-slide-content">
-                        <h1 className="modal-img-title" style={{fontSize:25,marginBottom:20,color:'#fff' }}>{img.title}</h1>
-                        <img src={img.src} alt={`screenshot-${index}`} className="modal-image" />
-                    </div>
-                </SwiperSlide>
-            ))}
-            </Swiper>
-        </div>
+        <h2 className="modal-title" style={{ color: "#fff" }}>
+          App Screenshots
+        </h2>
+
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={1}
+          loop={images.length > 1}
+          navigation
+          modules={[Navigation]}
+        >
+          {images.map((img, index) => (
+            <SwiperSlide key={index}>
+              <ImageWithLoader title={img.title} src={img.src} index={index} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
+  );
+};
 
+const ImageWithLoader = ({ src, title, index }) => {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <div className="modal-slide-content">
+      <h1 className="modal-img-title" style={{ fontSize: 25, marginBottom: 20, color: "#fff" }}>
+        {title}
+      </h1>
+
+      {loading && (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      )}
+
+      <img
+        src={src}
+        alt={`screenshot-${index}`}
+        className="modal-image"
+        style={{ display: loading ? "none" : "block" }}
+        onLoad={() => setLoading(false)}
+      />
+    </div>
   );
 };
 
